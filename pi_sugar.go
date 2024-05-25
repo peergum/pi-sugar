66995e193b1dc851af7a8904c194903d19193d70
Phil Hilger <phil@peergum.com>
2024-05-24 23:23:36 -0400

    basic functionality

--- pi_sugar.go
+++ pi_sugar.go
@@ -24,39 +24,53 @@ import (
 )
 
 type PiSugar struct {
-	voltage  int
-	charge   int
-	power    bool
-	charging bool
-	model    int
+	voltage     float64
+	charge      int
+	power       bool
+	charging    bool
+	model       int
+	temperature int
+	*rpio.I2cDevice
 }
+
+const (
+	piSugarI2CAddress = 0x57
+
+	powerReg         = 0x02
+	temperatureReg   = 0x04
+	voltageReg       = 0x22
+	batteryChargeReg = 0x2a
+	chargingStatusReg
+)
 
 var (
 	piSugar PiSugar
 )
 
-func Init() error {
-	if err := rpio.Open(); err != nil {
+func Init() (err error) {
+	if err = rpio.Open(); err != nil {
 		log.Printf("Can't open rpio %v", err)
 		return err
 	}
 
-	if err := rpio.I2cBegin(rpio.I2c1); err != nil {
+	if piSugar.I2cDevice, err = rpio.I2cBegin(rpio.I2c1, piSugarI2CAddress); err != nil {
 		log.Printf("Can't start I2C %v", err)
 		return err
 	}
+	piSugar.I2cSetSlaveAddress(0x57)
+	//piSugar.I2cSetBaudrate(110000)
 	return nil
 }
 
 func End() {
-	rpio.I2cEnd(rpio.I2c1)
+	piSugar.I2cEnd()
 }
 
 func NewPiSugar() (*PiSugar, error) {
 	return &piSugar, nil
 }
 
-func (piSugar *PiSugar) Voltage() int {
+func (piSugar *PiSugar) Voltage() float64 {
 	return piSugar.voltage
 }
 
@@ -73,5 +87,26 @@ func (piSugar *PiSugar) Power() bool {
 }
 
 func (piSugar *PiSugar) Refresh() {
-
+	var buf []byte = make([]byte, 2)
+	code := piSugar.I2cReadRegister(temperatureReg, buf, 1)
+	if code == 0 {
+		piSugar.temperature = int(buf[0]) - 40
+	}
+	code = piSugar.I2cReadRegister(voltageReg, buf, 2)
+	if code == 0 {
+		piSugar.voltage = float64(uint16(buf[0])<<8|uint16(buf[1])) / 1000
+	}
+	code = piSugar.I2cReadRegister(batteryChargeReg, buf, 1)
+	if code == 0 {
+		piSugar.charge = int(buf[0])
+	}
+	code = piSugar.I2cReadRegister(powerReg, buf, 1)
+	if code == 0 {
+		piSugar.power = buf[0]&0x80 != 0
+	}
+	Debug("T = %dºC, V = %.3fV, B = %d%%, P = %t",
+		piSugar.temperature,
+		piSugar.voltage,
+		piSugar.charge,
+		piSugar.power)
 }
